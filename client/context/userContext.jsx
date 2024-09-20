091b9859934872bd64724d9676618b54f13a86e0
Sincal200 <sincaljulio200@gmail.com>
2024-09-19 18:24:10 -0600

    Cambios

--- client/context/userContext.jsx
+++ client/context/userContext.jsx
@@ -1,22 +1,33 @@
+import { createContext, useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-import { createContext , useState, useEffect} from 'react';
 
 export const UserContext = createContext({});
 
-export function UserContextProvider({children}){
+export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
+    const location = useLocation();
+    const [prevPath, setPrevPath] = useState('');
+
     useEffect(() => {
-        if(!user){
-            axios.get('/profile').then(({data}) =>{
-                setUser(data);
-            })
+        const currentPath = location.pathname;
+        if ((!user) || prevPath === '/login' && currentPath === '/dashboard') {
+            const fetchUser = async () => {
+                try {
+                    const { data } = await axios.get('/profile');
+                    setUser(data);
+                } catch (error) {
+                    console.error('Error fetching user data:', error);
+                }
+            };
+            fetchUser();
         }
-    }, [])
+        setPrevPath(currentPath);
+    }, [location, prevPath, user]);
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
         </UserContext.Provider>
-    )
- 
+    );
 }
